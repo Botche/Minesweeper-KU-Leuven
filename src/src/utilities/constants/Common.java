6394429100d8f1b66e245a70a74f006d2085e9a6
PetarKTodorov <PetarKTodorov@gmail.com>
2022-03-07 15:40:54 +0100

    Refactor utils classes

--- src/src/utilities/constants/Common.java
+++ src/src/utilities/constants/Common.java
@@ -1,7 +1,9 @@
-package utilities;
+package utilities.constants;
 
-public final class Constants {
+public final class Common {
     public static int[] EASY_BORD_DIMENSIONS = new int[] { 8, 8 };
     public static int[] MEDIUM_BORD_DIMENSIONS = new int[] { 16, 16 };
     public static int[] HARD_BORD_DIMENSIONS = new int[] { 16, 30 };
+
+
 }

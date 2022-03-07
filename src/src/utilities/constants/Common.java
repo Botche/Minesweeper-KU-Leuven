581f662dbe045c4fe04aacae516f4bd50ae220f7
PetarKTodorov <PetarKTodorov@gmail.com>
2022-03-07 16:34:50 +0100

    Update Common.java

--- src/src/utilities/constants/Common.java
+++ src/src/utilities/constants/Common.java
@@ -1,9 +1,17 @@
 package utilities.constants;
 
 public final class Common {
+    public static int EASY_BORD_COUNT_OF_MINES = 10;
     public static int[] EASY_BORD_DIMENSIONS = new int[] { 8, 8 };
+
+    public static int MEDIUM_BORD_COUNT_OF_MINES = 40;
     public static int[] MEDIUM_BORD_DIMENSIONS = new int[] { 16, 16 };
+
+    public static int HARD_BORD_COUNT_OF_MINES = 99;
     public static int[] HARD_BORD_DIMENSIONS = new int[] { 16, 30 };
+
+
+
 
 
 }

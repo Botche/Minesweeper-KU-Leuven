7c6404b69b664610ae8413dc6693c8c4ec042820
Gabriel <47752724+Botche@users.noreply.github.com>
2022-03-07 16:30:43 +0100

    Add the game model to the view

--- src/src/App.java
+++ src/src/App.java
@@ -2,6 +2,7 @@ import model.Difficulty;
 //import model.Minesweeper;
 import model.Minesweeper;
 import model.PlayableMinesweeper;
+import utilities.constants.Common;
 import view.MinesweeperView;
 
 public class App {
@@ -9,13 +10,13 @@ public class App {
         MinesweeperView view = new MinesweeperView();
         //Uncomment the lines below once your game model code is ready; don't forget to import your game model 
         PlayableMinesweeper model = new Minesweeper();
-        
-        
+
         /**
             Your code to bind your game model to the game user interface
         */
-        
-        
+
         model.startNewGame(Difficulty.EASY);
+        view.setGameModel(model);
+        view.notifyNewGame(Common.EASY_BORD_DIMENSIONS[0], Common.EASY_BORD_DIMENSIONS[1]);
     }
 }
